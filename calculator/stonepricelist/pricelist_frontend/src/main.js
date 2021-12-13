0365d88dcecd1543f673d2734620d967364ae6d0
kiortir <84376533+kiortir@users.noreply.github.com>
2021-12-13 11:08:13 +0300

    static fix

--- calculator/stonepricelist/pricelist_frontend/src/main.js
+++ calculator/stonepricelist/pricelist_frontend/src/main.js
@@ -5,16 +5,34 @@ import App from './App.vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faChevronLeft, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronLeft, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faChevronLeft)
-library.add(faEllipsisV)
+library.add(faChevronLeft);
+library.add(faEllipsisV);
+
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + '=') {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
+axios.defaults.headers.common = { 'X-CSRFToken': csrftoken };
 
 const app = createApp(App);
 
 app.use(VueAxios, axios);
 
-app.component('font-awesome-icon', FontAwesomeIcon)
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
