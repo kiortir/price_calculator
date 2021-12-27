022aaddd73d83628b63e67a56a67b61abc314cc4
kiortir <84376533+kiortir@users.noreply.github.com>
2021-12-27 20:10:38 +0300

    ss

--- calculator/amoApi/models.py
+++ calculator/amoApi/models.py
@@ -19,11 +19,11 @@ class Lead(models.Model):
         max_length=10, null=True, blank=True, verbose_name='номер договора')
     contract_start_date = models.IntegerField(
         verbose_name='дата начала')
-    contract_duration = models.IntegerField(null=True,
+    contract_duration = models.IntegerField(null=True, default=0,
                                             verbose_name='срок по договору')
-    work_duration = models.IntegerField(null=True,
+    work_duration = models.IntegerField(null=True, default=0,
                                         verbose_name='срок изготовления')
-    work_start_date = models.IntegerField(null=True,
+    work_start_date = models.IntegerField(null=True, default=0,
                                           verbose_name='дата передачи в работу')
 
     status_id = models.IntegerField(
