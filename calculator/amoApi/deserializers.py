d22615df2dfe10830fe756a1cc017add2d38fa25
kiortir <84376533+kiortir@users.noreply.github.com>
2021-12-27 19:51:59 +0300

    test

--- calculator/amoApi/deserializers.py
+++ calculator/amoApi/deserializers.py
@@ -30,10 +30,13 @@ def webhook(lead_webhook):
     custom_fields_entries = CustomFields.objects.all()
     custom_fields_dict = {field.field_id: {'name': field.field_name, 'type': field.field_type} for field in
                           custom_fields_entries}
-
+    print(custom_fields_dict)
     lead_info = lead_webhook['leads']
+    print(lead_info)
     hook_type = list(lead_info.keys())[0]
+    print(hook_type)
     lead_fields = lead_info[hook_type][0]
+    print(lead_fields)
 
     lead_dict = {'lead_id': lead_fields['id']}
     lead_dict['status_id'] = lead_fields['status_id']
