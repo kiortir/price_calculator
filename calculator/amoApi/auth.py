1834f88ede2eb6a13e959668471c51a1159dfa4f
kiortir <84376533+kiortir@users.noreply.github.com>
2022-01-12 15:34:04 +0300

    auth

--- calculator/amoApi/auth.py
+++ calculator/amoApi/auth.py
@@ -14,6 +14,11 @@ AUTH_HEADERS["Content-Type"] = "application/json"
 
 def setTokens(tokens: dict):
     token_entry = Token.objects.get(id=1)
+    try:
+        if len(tokens.keys) == 0:
+            raise Exception
+    except Exception:
+        logging.exception('message')
     for field, value in tokens.items():
         setattr(token_entry, field, value)
     try:
