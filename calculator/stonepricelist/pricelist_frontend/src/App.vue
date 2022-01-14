bfedda49fbcd91ba1727bf324ad6a0ae51c9b635
kiortir <84376533+kiortir@users.noreply.github.com>
2022-01-14 14:09:11 +0300

    stones

--- calculator/stonepricelist/pricelist_frontend/src/App.vue
+++ calculator/stonepricelist/pricelist_frontend/src/App.vue
@@ -132,6 +132,7 @@ export default {
               name: stone.name,
               code: stone.code,
               info: "Не нашелся(",
+              equivalents: stone.equivalents,
             };
           }
         })
