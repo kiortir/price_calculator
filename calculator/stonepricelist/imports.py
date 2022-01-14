24f93df6a10ced8676aae40454aca0937572f4fd
kiortir <84376533+kiortir@users.noreply.github.com>
2022-01-14 15:39:50 +0300

    import

--- calculator/stonepricelist/imports.py
+++ calculator/stonepricelist/imports.py
@@ -5,20 +5,18 @@ from .models import AcrylicCollection
 class toCollection(widgets.Widget):
 
     def clean(self, value: str, row=None, *args, **kwargs):
-        print(value)
         manufacturer, collection = value.split('/')
 
         collection_list = AcrylicCollection.objects.filter(
             manufacturer__name=manufacturer)
-        print(collection_list)
         print(collection, collection in list(
             map(lambda x: x.name, collection_list)))
         requested_collection = collection_list.get(name=collection)
         return requested_collection
 
-    # def render(self, value, obj=None):
-    #     print(value.manufacturer)
-    #     return f'{value.manufacturer}/{value.name}'
+    def render(self, value, obj=None):
+        print(value.manufacturer)
+        return f'{value.manufacturer}/{value.name}'
 
 
 # class Equivalents(widgets.ManyToManyWidget):
