defdeed06a4709a1bb65d79ddbdda00c6c92d34d
kiortir <84376533+kiortir@users.noreply.github.com>
2022-03-18 16:32:39 +0300

    favicon

---
+++ .vscode/scripts/django.cmd
@@ -0,0 +1,3 @@
+@echo off
+CALL C:\Users\Admin\Documents\unirock\venv\Scripts\Activate.bat
+python C:\Users\Admin\Documents\unirock\calculator\manage.py runserver
---
+++ .vscode/scripts/vite.cmd
@@ -0,0 +1,2 @@
+@echo off
+yarn --cwd C:\Users\Admin\Documents\unirock\calculator\main\frontend-vite dev
