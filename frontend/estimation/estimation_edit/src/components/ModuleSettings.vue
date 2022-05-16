0fefc273d0d19cf33e5559013a92ebc0ab9e56ea
kiortir <84376533+kiortir@users.noreply.github.com>
2022-05-16 11:22:25 +0300

    pricelist fix

---
+++ frontend/estimation/estimation_edit/src/components/ModuleSettings.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+    settings: object
+}>()
+
+</script>
+
+<template>
+    <div class="settings text-xl">Настройки</div>
+    <el-form :model="settings" class="pl-3">
+        <el-form-item label="Можно делать скидку">
+            <el-switch v-model="settings.discountable"></el-switch>
+        </el-form-item>
+        <el-form-item label="Можно дублировать">
+            <el-switch v-model="settings.duplicable"></el-switch>
+        </el-form-item>
+    </el-form>
+
+</template>
---
+++ frontend/estimation/estimation_front/src/components/Offer.vue
@@ -0,0 +1,3 @@
+<template>
+    <el-table></el-table>
+</template>
